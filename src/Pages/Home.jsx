64ecab02695d3912ca03368161ed7447b3a9228d
henrikgui <guilherme10hg@gmail.com>
2022-08-07 19:34:14 -0300

    Estiliando pagina home e register

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,45 +2,38 @@ import '../App.css';
 
 function Home() {
   return (
-<body class="h-screen w-screen justify-center flex items-center bg-esmerald-100 ">
+<body class="h-screen w-screen justify-center flex items-center bg-teal-600 ">
 
-  <div class="max-w-md">
+  <div class="bg-white w-96 h-80 shadow-md rounded-md p-6 space-y-4">
     
-   <h1 class="text-lg font-bold text-center mb-2">Login</h1>
-   
-    <div class="bg-white shadow-md rounded-md p-6 space-y-4"> 
+       <h1 class="text-lg font-bold text-center text-teal-800">Login</h1>
 
-       <label for="emailLogin"
+       <div for="emailLogin"
         type="text"
-        class="text-sm text-gray-800 ">E-mail</label>
+        class="text-sm text-teal-800">E-mail
          <input
           id="emailLogin"
           type="email"
-          class="w-full bg-gray-100 border border-gray-700 focus:border-blue-500 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+          class="w-full bg-gray-100 border border-teal-900 focus:border-teal-400 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+       </div>
 
-       <label 
-        for="passwordLogin" 
-        class="text-sm text-gray-800">Senha</label>
+       <div for="passwordLogin" 
+        class="text-sm text-teal-800 ">Senha
          <input
           id="passwordLogin"
           type="password"
-          class="w-full bg-gray-100 border border-gray-700 focus:border-blue-500 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+          class="w-full  bg-gray-100 border border-teal-900 focus:border-teal-400 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+      </div>
       
-      <div class="">
-
-       <button class="w-full flex flex-row text-white text-sm bg-blue-500 hover:bg-blue-600 p-1.5 rounded-md transition duration-200 ease-in-out "><p class="flex-1">Login</p>
+      <button class="w-full flex flex-row text-white text-sm bg-teal-900 hover:bg-teal-800 p-1.5 rounded-md transition duration-200 ease-in-out "><p class="flex-1">Login</p>
         <svg xmlns="http://www.w3.org/2000/svg" class="flex-none h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
          <path stroke-linecap="round" stroke-linejoin="round" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
         </svg> 
-        </button>
+      </button>
        
-      </div>
-
        <div>
-        <a href="./register" class="text-xs  text-gray-600 hover:text-blue-600  hover:underline">Cadastrar</a>
+        <a href="./register" class="text-xs  text-teal-900 hover:text-teal-600  hover:underline">Cadastrar</a>
        </div>
-
-    </div>
 
   </div>
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,53 +2,48 @@ import '../App.css'
 
 function Register(){
 return(
-<body class="h-screen w-screen flex justify-center items-center bg-es "> 
+<body class="h-screen w-screen justify-center flex items-center bg-teal-600">
 
-<div class="max-w-md">
-  
- <h1 class="text-lg font-bold text-center mb-2">Cadastro</h1>
- 
-  <div class="bg-white shadow-md rounded-md p-6 space-y-4"> 
-    <label for="nameRegister"
-      type="text"
-      class="text-sm text-gray-800 ">Nome Completo</label>
+
+<div class="bg-white shadow-md w-96 rounded-md p-6 space-y-4"> 
+
+    <h1 class="text-lg font-bold text-center mb-2 text-teal-800">Cadastro</h1>
+
+    <div for="nameRegister"
+         type="text"
+         class="text-sm text-gray-800 ">Nome completo
        <input
         id="nameRegister"
         type="name"
-        class="placeholder:Name w-full bg-gray-100 border border-gray-700 focus:border-blue-500 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+        class="placeholder:Name w-full bg-gray-100 border border-teal-900 focus:border-teal-400 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+    </div>
 
-     <label for="emailRegister"
-      type="text"
-      class="text-sm text-gray-800 ">E-mail</label>
+    <div for="emailRegister"
+         type="text"
+         class="text-sm text-gray-800 ">E-mail
        <input
         id="emailRegister"
         type="email"
-        class="w-full bg-gray-100 border border-gray-700 focus:border-blue-500 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+        class="w-full bg-gray-100 border border-teal-900 focus:border-teal-400 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
+    </div>
 
-     <label 
-      for="passwordRegister" 
-      class="text-sm text-gray-800">Senha</label>
+    <div for="passwordRegister" 
+        class="text-sm text-gray-800">Senha
        <input
         id="passwordRegister"
         type="password"
-        class="w-full bg-gray-100 border border-gray-700 focus:border-blue-500 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
-    
-    <div class="">
-
-     <button class="w-full flex flex-row text-white text-sm bg-blue-500 hover:bg-blue-600 p-1.5 rounded-md transition duration-200 ease-in-out "><p class="flex-1">Cadastrar</p>
-      <svg xmlns="http://www.w3.org/2000/svg" class=" flex-none h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-       <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-      </svg>
-      </button>
-
+        class="w-full bg-gray-100 border border-teal-900 focus:border-teal-400 rounded px-2 py-1 text-base placeholder-gray-400 focus:outline-none transition duration-200 ease-in-out"/>
     </div>
+    
+    <button class="w-full flex flex-row text-white text-sm bg-teal-900 hover:bg-teal-800 p-1.5 rounded-md transition duration-200 ease-in-out "><p class="flex-1">Cadastrar</p>
+      <svg xmlns="http://www.w3.org/2000/svg" class=" flex-none h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+      </svg>
+    </button>
 
     <div>
-      <a href="./"class="text-xs  text-gray-600 hover:text-blue-600  hover:underline"> 
-        Voltar </a>
+      <a href="./" class="text-xs text-teal-900 hover:text-teal-600 hover:underline">Voltar</a>
     </div>
-
-  </div>
 
 </div>
 
